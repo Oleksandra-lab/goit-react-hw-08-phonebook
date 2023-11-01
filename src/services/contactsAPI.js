d4720241d4417a267084643b16444f96cc5ad3c4
Oleksandra-lab <127051632+Oleksandra-lab@users.noreply.github.com>
2023-11-01 23:20:24 +0200

    Registration requests

--- src/services/contactsAPI.js
+++ src/services/contactsAPI.js
@@ -1,4 +1,35 @@
-import axios from 'axios'
+import axios from 'axios';
+
+const phonebookInstance = axios.create({
+  baseUrl: 'https://connections-api.herokuapp.com/',
+  headers: {
+    Authorization: 'Bearer...',
+  },
+});
+
+export const requestRegister = async formData => {
+  const { data } = await phonebookInstance.post('​/users​/signup', formData);
+
+  return data;
+};
+
+export const requestLogin = async formData => {
+  const { data } = await phonebookInstance.post('​/users​/login', formData);
+
+  return data;
+};
+
+export const requestLogout = async () => {
+  const { data } = await phonebookInstance.post('​/users​/logout');
+
+  return data;
+};
+
+export const requestRefreshUser = async () => {
+  const { data } = await phonebookInstance.get('​/users​/current');
+
+  return data;
+};
 
 export const fetchContacts = async () => {
     const {data} = await axios.get (
@@ -8,16 +39,17 @@ export const fetchContacts = async () => {
     return data
 }
 
-export const deleteContact = async (id) => {
-    const {data} = await axios.delete(
-        `https://6536c8b2bb226bb85dd29f68.mockapi.io/api/v1/contacts/${id}`
-    );
-    return data
-}
+export const deleteContact = async id => {
+  const { data } = await axios.delete(
+    `https://6536c8b2bb226bb85dd29f68.mockapi.io/api/v1/contacts/${id}`
+  );
+  return data;
+};
 
-export const addContact = async (newContact) => {
-    const {data} = await axios.post(
-        `https://6536c8b2bb226bb85dd29f68.mockapi.io/api/v1/contacts/`, newContact
-    );
-    return data
-}
+export const addContact = async newContact => {
+  const { data } = await axios.post(
+    `https://6536c8b2bb226bb85dd29f68.mockapi.io/api/v1/contacts/`,
+    newContact
+  );
+  return data;
+};
